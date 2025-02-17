4a7ffbe23f678e70248e7decc09ecef71e0fe604
rafi00000 <delwar2021bd@gmail.com>
2025-02-17 17:21:08 +0600

    Updated error.

--- event/forms.py
+++ event/forms.py
@@ -1,8 +1,8 @@
-from django.forms import ModelForm, forms
+from django import forms
 from event.models import Event
 
 class CreateEventMixin:
-
+    default_classes = "border border-2 border-black p-2 mb-2 rounded-md w-full"
     def apply_form_style(self):
         for field_name, field in self.fields.items():
             if isinstance(field.widget, forms.TextInput):
@@ -24,8 +24,14 @@ class CreateEventMixin:
                 field.widget.attrs.update({
                     'class': self.default_classes
                 })
+            elif isinstance(field.widget, forms.ModelChoiceField):
+                field.widget.attrs.update({
+                    'class': f"w-full"
+                })
+            
 
-class CreateEventForm(CreateEventMixin ,ModelForm):
+
+class CreateEventForm(CreateEventMixin ,forms.ModelForm):
     class Meta:
         model = Event
         fields = "__all__"
